9eea1555718b6770c2137b2d57115bcfec2b291f
cleitonrs <rscleitonrs@gmail.com>
2023-08-03 11:42:01 -0300

    navbar component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import CartIcon from './CartIcon'
 
 const links = [
   { id: 1, title: "Início", url: "/" },
@@ -21,23 +22,26 @@ const Menu = () => {
       {!openMenu ? (
         <Image src='/open.png' alt='' width={20} height={20} onClick={() => setOpenMenu(true)}/> 
       ) : (
-        <Image src='/close.png' alt='' width={20} height={20} onClick={() => setOpenMenu(false)}/>
+        <Image src='/close.png' alt='' width={20} height={20} onClick={() => setOpenMenu(false)}
+        />
       )}
-      <div className='bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10'>
+      {openMenu && (
+         <div className='bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10'>
         {links.map(item => (
-          <Link href={item.url} key={item.id}>
+          <Link href={item.url} key={item.id} onClick={() => setOpenMenu(false)}>
             {item.title}
           </Link>
         ))}
         {!user ? (
-          <Link href="/login">Login</Link>
+          <Link href="/login" onClick={() => setOpenMenu(false)}>Login</Link>
         ) : (
-          <Link href="/orders">Pedidos</Link>
+          <Link href="/orders" onClick={() => setOpenMenu(false)}>Pedidos</Link>
         )}
-        <Link href="/cart">
-          
+        <Link href="/cart" onClick={() => setOpenMenu(false)}>
+          <CartIcon/>
         </Link>
       </div>
+      )}
     </div>
   )
 }
