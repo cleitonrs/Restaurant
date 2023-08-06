e988fbb3e8d6a2f731e9f43596f2e59bab86fe23
cleitonrs <rscleitonrs@gmail.com>
2023-08-06 09:23:21 -0300

    single product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,8 +1,20 @@
+import { singleProduct } from '@/data'
+import Image from 'next/image'
 import React from 'react'
 
 const SingleProductPage = () => {
   return (
-    <div>SingleProductPage</div>
+    <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:fle-row'>
+      {/* Image Container */}
+      {singleProduct.img ? (<div className='relative'>
+        <Image src={singleProduct.img} alt='' className='object-contain' fill />
+      </div>) : null}
+      {/* Text Container */}
+      <div className=''>
+        <h1>{singleProduct.title}</h1>
+        <p>{singleProduct.desc}</p>
+      </div>
+    </div>
   )
 }
 
