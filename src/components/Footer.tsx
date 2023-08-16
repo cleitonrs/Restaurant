9d89433c6fb9addc149146dc32d437853ea5f471
cleitonrs <rscleitonrs@gmail.com>
2023-08-16 20:14:40 -0300

    change site title

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between'>
-      <Link href="/" className='font-bold text-xl'>L'Italia</Link>
+      <Link href="/" className='font-bold text-xl'>LaItalia</Link>
       <p className='uppercase text-sm'>&copy; Todos os direitos reservados</p>
     </div>
   )
